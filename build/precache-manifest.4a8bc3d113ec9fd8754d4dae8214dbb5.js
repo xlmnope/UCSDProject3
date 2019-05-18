5babf9c7241a094bdd412b3636434a22e4352c45
kirsnik <gwingster@gmail.com>
2019-05-18 11:43:45 -0700

    added cart

---
+++ build/precache-manifest.4a8bc3d113ec9fd8754d4dae8214dbb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03fda5d9b75c6c346d6aa0f444e7824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9a2586b078f41cf302",
+    "url": "/static/css/main.f2fab250.chunk.css"
+  },
+  {
+    "revision": "6e8b4e0067922328afc0",
+    "url": "/static/js/2.bfbb5b11.chunk.js"
+  },
+  {
+    "revision": "4e9a2586b078f41cf302",
+    "url": "/static/js/main.9ac6010e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
