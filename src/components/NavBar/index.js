1035e5f703368158a30ed3c0beb732a20c72c0fe
xlmnope <lmgiroud@gmail.com>
2019-05-21 13:50:29 -0700

    removing about and top shopping cart icon for now

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -10,18 +10,18 @@ function NavBar(props) {
         <span className="navbar-toggler-icon"></span>
     </button>
     <div className="navbar-collapse collapse" id="collapsingNavbar">
-        <ul className="navbar-nav">
+        {/* <ul className="navbar-nav">
             <li className="nav-item active">
                 <a className="nav-link" href="/">About</a>
             </li>
-        </ul>
-        <ul className="navbar-nav ml-auto">
+        </ul> */}
+        {/* <ul className="navbar-nav ml-auto">
             <li className="nav-item">
                 <a className="nav-link" href="/" data-target="#myModal" data-toggle="modal">    
                   <i className="fas fa-shopping-cart"></i>
                 </a>
             </li>
-        </ul>
+        </ul> */}
     </div>
     </div>
   </nav>
