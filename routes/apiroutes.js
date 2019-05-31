dae2ee56ffacea9cc77783ec8f9052a338e4fc6a
kirsnik <gwingster@gmail.com>
2019-05-31 02:13:46 -0700

    added admin mock page

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -87,6 +87,17 @@ module.exports = function (app) {
   });
 
 
+  //Admin section
+
+  app.post("/api/adminLogin", passport.authenticate("local"), function (req, res) {
+    console.log('test');
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the users back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/adminPage");
+  });
+
+
 
 
 
