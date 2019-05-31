79729c3714684b9a194b2524f21e6b7190434a15
xlmnope <lmgiroud@gmail.com>
2019-05-31 15:24:54 -0700

    styled the cart card

--- src/components/CheckoutCard/index.js
+++ src/components/CheckoutCard/index.js
@@ -15,11 +15,29 @@ function CheckoutCard(props) {
 
     <ul className="list-group list-group-flush">
       {props.cart.length > 0 ? props.cart.map(cartitem => (
-         <li className="list-group-item">
-            <select>
+         <li className="list-group-item itemrow">
+            <div className="selectamountcontainer">
+              <select className="selectamount">
               {props.renderOptions(cartitem.count)}
-            </select>
-             {cartitem.name} {cartitem.price}  
+              </select>
+            </div>
+            <div className="itemandoptions">
+              <div className="item">
+              {cartitem.name}
+              </div>
+              <div className="optionscontainer">
+                <div className="optionscart">
+                  {/* {this.renderSelectedOptions} */}
+                  {cartitem.options}  
+                </div> 
+              </div>
+              
+              
+            </div>
+            <div className="pricecart">
+              {cartitem.price}  
+            </div>
+            
           </li>
       )) : <li className="list-group-item empty">Add items to your cart and they’ll appear herename.</li>}
     </ul>
